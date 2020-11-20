246f8c0de2fb08559462545f603481252af6fb0c
OB-PeteReeves <56554204+OB-PeteReeves@users.noreply.github.com>
2020-11-20 09:29:32 +0000

    Components Created

---
+++ application/ScheduleDownloader/ScheduleDownloader-rt/src/com/optimised_buildings_ltd/ScheduleDownloader/BSpecialEventImporter.java
@@ -0,0 +1,22 @@
+package com.optimised_buildings_ltd.ScheduleDownloader;
+
+import javax.baja.nre.annotations.NiagaraType;
+import javax.baja.sys.*;
+
+@NiagaraType
+
+public class BSpecialEventImporter extends BComponent {
+/*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+/*@ $com.optimised_buildings_ltd.ScheduleDownloader.BSpecialEventImporter(2979906276)1.0$ @*/
+/* Generated Fri Nov 20 09:28:14 GMT 2020 by Slot-o-Matic (c) Tridium, Inc. 2012 */
+
+////////////////////////////////////////////////////////////////
+// Type
+////////////////////////////////////////////////////////////////
+  
+  @Override
+  public Type getType() { return TYPE; }
+  public static final Type TYPE = Sys.loadType(BSpecialEventImporter.class);
+
+/*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
+}
